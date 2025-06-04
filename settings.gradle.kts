9302d37b9e9b999e4b7a1a2a73e8ff0e3b8326d9
youguxuanhe <x269li@uwaterloo.ca>
2025-06-04 13:28:12 -0400

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ImageRecognizer"
+
+include(":app")
+include(":opencv")
+
+project(":opencv").projectDir = File("/Users/eloise/Downloads/OpenCV-android-sdk/sdk/java")
+
